a0b22b2ada6c4568abc39f4a62afa71ea07761cd
Nupur268 <72887111+Nupur268@users.noreply.github.com>
2021-12-12 22:08:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+function myFunction(){
+    var x = document.getElementById("myInput");
+if (x.type === "password") {
+x.type = "text";
+
+} else {
+x.type = "password";
+}
+}
+function mFunction(){
+    var x = document.getElementById("pass-hidden");
+if (x.type === "password") {
+x.type = "text";
+
+} else {
+x.type = "password";
+}
+}
+
+var x=document.getElementById('login');
+var y=document.getElementById('register');
+var z=document.getElementById('btn');
+
+function register()
+{
+    x.style.left='-400px';
+    y.style.left='50px';
+    z.style.left='110px';
+
+}
+function login()
+{
+    x.style.left='50px';
+    y.style.left='450px';
+    z.style.left='0px';
+}
+
